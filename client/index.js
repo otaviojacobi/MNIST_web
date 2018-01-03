bd60deab5b60caedba2826e18a07afd19ec5fce3
i860600 <o.jacobi@sap.com>
2018-01-03 19:58:52 +0200

    initial commit

---
+++ client/index.js
@@ -0,0 +1,33 @@
+let click = false;
+let xCoord, yCoord = "";
+let canvas = document.getElementById("canvas");
+let cntx = canvas.getContext("2d");
+cntx.strokeStyle = "black";
+cntx.lineWidth = 4;
+cntx.lineCap = "round";
+cntx.fillStyle = "#fff";
+cntx.fillRect(0, 0, canvas.width, canvas.height);
+
+
+canvas.addEventListener('mousedown', event => {
+    click = true;
+    cntx.save();
+    xCoord = event.pageX - canvas.offsetLeft;
+    yCoord = event.pageY - canvas.offsetTop
+});
+
+canvas.addEventListener('mouseup', event => { click = false; });
+
+canvas.addEventListener('click', event => { click = false; });
+
+canvas.addEventListener('mousemove', event => {
+    if (click == true) {
+        cntx.beginPath();
+        cntx.moveTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
+        cntx.lineTo(xCoord, yCoord);
+        cntx.stroke();
+        cntx.closePath();
+        xCoord = event.pageX - canvas.offsetLeft;
+        yCoord = event.pageY - canvas.offsetTop
+    }
+});
